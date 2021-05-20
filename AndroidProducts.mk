180d741a9c93194e37181b4f3603f3804cbe0c81
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-05-20 08:00:41 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ssos_ysl.mk
+    $(LOCAL_DIR)/palladium_ysl.mk
 
 COMMON_LUNCH_CHOICES := \
-    ssos_ysl-user \
-    ssos_ysl-userdebug \
-    ssos_ysl-eng
+    palladium_ysl-user \
+    palladium_ysl-userdebug \
+    palladium_ysl-eng
